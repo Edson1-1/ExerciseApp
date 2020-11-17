9ef84b1d402899bc398250d6c355c1e93eb0a6ea
Edson Reginold <edson@Edsons-MacBook-Pro.local>
2020-11-17 23:07:42 +0530

    handler moved to controller

--- routes/adminRoutes/user.js
+++ routes/adminRoutes/user.js
@@ -1,26 +1,14 @@
 //CRUD user admin API - should contain /admin/api in the path
 
-const bcrypt = require('bcrypt')
 const Joi = require('@hapi/joi')
+const userController = require('../../controllers/userController')
 
 const baseAddress = '/admin/api/user'
 module.exports = [
     {   //user sign up
         method: 'POST',
         path: baseAddress+'/register',
-        handler: async(request, h) => {
-            const password = request.payload.password
-            const hashedPassword = bcrypt.hashSync(password, parseInt(process.env.SALT));
-            const user = { 
-                name : request.payload.name,
-                hashedPassword : hashedPassword,
-                email: request.payload.email,
-                role_id: request.payload.role_id,
-            }
-            console.log(hashedPassword)
-            console.log(user)
-            return user
-        },
+        handler: userController.userRegister,
         options: {
             auth: false,
             validate: {
@@ -36,27 +24,23 @@ module.exports = [
     {   //user login
         method: 'POST',
         path: baseAddress+'/login',
-        handler: async (request, h) => {
-            const hashedPassword = "$2b$10$JaHWv.NSN8ctdYWaJdAxHuPkmjklO6g3zfT7J4AdTi.sRRg9dArUK"
-            const user = {
-                id: 1,
-                name: 'Jon Doe',
-                password: "endurance",
-                email: 'ssss@email.com',
-                role_id: 1
-              }
-                
-              if(bcrypt.compareSync(user.password, hashedPassword)){
-                request.cookieAuth.set({ id: user.id });
-                return "logged in"
-               }else{
-                   return "not logged in"
-               }  
-        },
+        handler: userController.userLogin,
         options: {
+            validate:{
+                payload: Joi.object({
+                    name: Joi.string().min(3),
+                    password: Joi.string().required(),
+                    role_id: Joi.required()
+                })
+            },
             auth: {
                 mode: 'try'
             }
         }   
+    },
+    {   //getAll users
+        method: 'GET',
+        path: baseAddress+'/all',
+        handler: userController.getAll
     }
 ]
