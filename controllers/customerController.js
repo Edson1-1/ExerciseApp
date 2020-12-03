34bef692ca55c420d037d6ffa0dea4d2655fb2c9
Edson Reginold <edson@Edsons-MacBook-Pro.local>
2020-12-03 12:28:32 +0530

    write data directly into map table

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,6 +6,14 @@ module.exports = {
     logExercise: async(request, h) => {
         const id = request.auth.credentials.id //user_id
         console.log("user id: ",id)
+        // write directly into map table
+        try{
+            const exerciseMap = await models.UserExercise.create({ user_id: id, exercise_id: request.payload.id, date: request.payload.date, duration: request.payload.duration})
+            return h.response({ message: 'succesfullly logged exercise', exerciseMap}).code(200)
+        }catch(err){
+            console.log(err.toString())
+            return h.response(err.toString()).code(400)
+        }
 
         try{
         user = await models.User.findByPk(id); //get User
@@ -19,13 +27,13 @@ module.exports = {
                 const newExercise = await models.Exercise.findByPk(e_id); //get Exercise
                 console.log(JSON.stringify(newExercise))
                 const userExerciseMap = await user.addExercise(newExercise, { through: 
-                    { date: exercise.date, start_time: exercise.startTime, end_time: exercise.endTime}
+                    { date: exercise.date, duration: exercise.duration}
                 }) /* create map b/w User and Exercise */
                 console.log(userExerciseMap)
                 return userExerciseMap
             }catch(err){
                 console.log(err.toString())
-                return ({message: err.toString()})
+                return h.response(err.toString()).code(400)
             }
             
     },
@@ -39,17 +47,27 @@ module.exports = {
             return deletedExercise
         }catch(err){
             console.log(err.toString())
-            return {message: err.toString()}
+            return h.response(err.toString()).code(400)
         }
     },
     getAllExercises: async(request, h) => {
         const user_id = request.auth.credentials.id
+        // try{
+        //     const userExerciseMap = await models.UserExercise.findAll({where: {id: user_id}, include: {model: models.Exercise, as: 'exercises'} })
+        //         console.log(JSON.stringify(userExerciseMap))
+        //         return userExerciseMap
+        // }catch(err){
+        //     console.log(err.toString())
+        //     return h.response(err.toString()).code(400)
+        // }
         try{
-            const userExerciseMap = await models.User.findOne({where: {id: user_id}, include: models.Exercise})
+            const userExerciseMap = await models.User.findOne({where: {id: user_id}, 
+                include: [models.Exercise, { model: models.User, as: 'trainers'}]})
+            console.log("got all exercises map")
             return userExerciseMap
         }catch(err){
             console.log(err.toString())
-            return {message: err.toString()}
+            return h.response(err.toString()).code(400)
         }
     }
 
